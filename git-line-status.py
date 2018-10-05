f0977e55068cc4b517e388104930b6918d8819fe
Joe Haines <hello@joehaines.co.uk>
2018-10-05 19:21:26 +0100

    Working version!

--- git-line-status.py
+++ git-line-status.py
@@ -4,53 +4,70 @@ import sublime
 import sublime_plugin
 import subprocess
 
-# TODO actually check if a line was added or deleted
-# TODO run automatically on save
-class GitLineStatusCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    self.view.erase_regions('git-line-status-added')
-    self.view.erase_regions('git-line-status-deleted')
+type_data = {
+  'added': ('git-line-status-added', 'markup.inserted'),
+  'deleted': ('git-line-status-deleted', 'markup.deleted'),
+  'modified': ('git-line-status-modified', 'comment.line')
+}
 
-    directory = self.view.window().extract_variables()['folder']
+hunk_regex = r'^@@ \-(?:\d+),?(\d*) \+(\d+),?(\d*) @@'
+
+class GitLineStatusCommand(sublime_plugin.EventListener):
+  def on_post_save_async(self, view):
+    variables = view.window().extract_variables()
+
+    if not 'folder' in variables:
+      return
+
+    directory = variables['folder']
 
     os.chdir(directory)
 
-    hunk_regex = r'^@@ -\d+(?:,\d+)? \+(\d+)(?:,(\d+))?\ @@'
-
     try:
-      output = subprocess.check_output([
+      diff = subprocess.check_output([
         'git',
         'diff',
         '-U0', # 0 lines of context as we only care about changed lines
         '--no-color',
-        self.view.file_name()
+        view.file_name()
       ]).decode()
 
-      for line in output.split('\n'):
-        print(line)
-        is_hunk = match = re.search(hunk_regex, line)
+      added = []
+      deleted = []
+      modified = []
 
-        if is_hunk:
-          print('at line number "', match.group(1), '" added "', match.group(2), '" lines')
-          self.added(
-            int(match.group(1)) - 1,
-            int(match.group(2))
-          )
+      for hunk in re.finditer(hunk_regex, diff, re.MULTILINE):
+        old_line_count = int(hunk.group(1) or 1)
+        first_line = int(hunk.group(2))
+        new_line_count = int(hunk.group(3) or 1)
+
+        if old_line_count == 0:
+          added += range(first_line, first_line + new_line_count)
+        elif new_line_count == 0:
+          deleted.append(first_line + 1)
+        else:
+          modified += range(first_line, first_line + new_line_count)
+
+      self.add_icon_to_lines(view, 'added', added)
+      self.add_icon_to_lines(view, 'deleted', deleted)
+      self.add_icon_to_lines(view, 'modified', modified)
     except subprocess.CalledProcessError as error:
       print('OH NO!!!!!', error.returncode)
       print(error.output)
 
-  def added(self, first_line, number_of_lines):
+  def add_icon_to_lines(self, view, type, lines):
     regions = []
 
-    for line_number in range(first_line, first_line + number_of_lines):
-      point = self.view.text_point(line_number, 0)
-      regions.append(self.view.line(point))
+    for line_number in lines:
+      point = view.text_point(line_number - 1, 0)
+      regions.append(view.line(point))
 
-    self.view.add_regions(
-      'git-line-status-added',
-      regions + self.view.get_regions('git-line-status-added'),
-      'markup.inserted',
+    key, scope = type_data[type]
+
+    view.add_regions(
+      key,
+      regions,
+      scope,
       'dot',
       sublime.HIDDEN | sublime.PERSISTENT
     )
